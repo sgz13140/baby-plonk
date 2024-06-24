af002de9af7f52f66427283ec031711af5ae13a6
jessica <jessica.sun@tron.network>
2024-06-24 22:19:41 +0800

    finish todo part

--- setup.py
+++ setup.py
@@ -29,7 +29,9 @@ class Setup(object):
         # powers_of_x[i] =  b.G1 * tau**i = powers_of_x[i - 1] * tau
         # TODO: generate powers_of_x
         # reference: https://github.com/sec-bit/learning-zkp/blob/master/plonk-intro-cn/5-plonk-polycom.md
-
+        powers_of_x[0] = b.G1
+        for i in range(1, powers):
+            powers_of_x[i] = b.multiply(powers_of_x[i - 1], tau)
         print("Generated G1 side, X^1 point: {}".format(powers_of_x[1]))
 
         X2 = b.multiply(b.G2, tau)
